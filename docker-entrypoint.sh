1ea766ef2c39b79c9669419dd8a1de640c94392d
Gustavo dos Anjos <lalodosanjos@gmail.com>
2025-06-22 19:31:14 -0300

    fix(entrypoint): ensure default config population and script compatibility
    - Update entrypoint to repopulate /usr/local/etc/privoxy from /opt/privoxy-default if empty.
    - Ensure all scripts use LF line endings and are executable.
    - Add step to copy default config to /opt/privoxy-default during build.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 set -e
 privman --init
+
+
+if [ ! -e /usr/local/etc/privoxy/config ] || [ -z "$(ls -A /usr/local/etc/privoxy)" ]; then
+  echo "[entrypoint] void config, populating defaults..."
+  cp -a /opt/privoxy-default/* /usr/local/etc/privoxy/
+fi
+
 exec "$@"
