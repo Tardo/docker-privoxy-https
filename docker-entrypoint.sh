f5d63bf692828fd667f7ba82cb9c480733f132d1
Alexandre D. Díaz <dev@redneboa.es>
2025-08-11 03:23:40 +0200

    fix: enable all tests

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,27 +12,27 @@ if [ ! -e /usr/local/etc/privoxy/config ] || [ -z "$(ls -A /usr/local/etc/privox
   cp -a /opt/privoxy-default/* /usr/local/etc/privoxy/
   cp /usr/local/etc/privoxy/config.orig /usr/local/etc/privoxy/config
   sed -i \
-    -e 's/^confdir .+/confdir \/usr\/local\/etc\/privoxy/' \
-    -e 's/^templdir .+/templdir \/usr\/local\/etc\/privoxy\/templates/' \
+    -e 's/^confdir .*/confdir \/usr\/local\/etc\/privoxy/' \
+    -e 's/^templdir .*/templdir \/usr\/local\/etc\/privoxy\/templates/' \
     -e '/^actionsfile user.action/a actionsfile privman-rules\/user.action\nactionsfile ab2p.system.action\nactionsfile ab2p.action' \
     -e '/^filterfile user.filter/a filterfile privman-rules\/user.filter\nfilterfile ab2p.system.filter\nfilterfile ab2p.filter' \
-    -e 's/^#debug     1.+/debug     1/' \
-    -e 's/^#debug   512.+/debug   512/' \
-    -e 's/^#debug  1024.+/debug  1024/' \
-    -e 's/^#debug  8192.+/debug  8192/' \
-    -e 's/^listen-address .+/listen-address  0.0.0.0:${PRIVOXY_PORT}/' \
-    -e 's/^enforce-blocks .+/#enforce-blocks 0/' \
-    -e 's/^buffer-limit .+/buffer-limit 25600/' \
-    -e 's/^keep-alive-timeout .+/keep-alive-timeout 120/' \
-    -e 's/^tolerate-pipelining .+/tolerate-pipelining 0/' \
-    -e 's/^socket-timeout .+/socket-timeout 30/' \
-    -e 's/^#max-client-connections .+/max-client-connections 256/' \
-    -e 's/^#listen-backlog .+/listen-backlog 128/' \
-    -e 's/^#ca-directory .+/ca-directory \/usr\/local\/etc\/privoxy\/CA/' \
-    -e 's/^#ca-cert-file .+/ca-cert-file privoxy-ca-bundle.crt/' \
-    -e 's/^#ca-key-file .+/ca-key-file cakey.pem/' \
-    -e 's/^#certificate-directory .+/certificate-directory \/usr\/local\/etc\/privoxy\/certs/' \
-    -e 's/^#trusted-cas-file .+/trusted-cas-file trustedCAs.pem/' \
+    -e 's/^#debug     1.*/debug     1/' \
+    -e 's/^#debug   512.*/debug   512/' \
+    -e 's/^#debug  1024.*/debug  1024/' \
+    -e 's/^#debug  8192.*/debug  8192/' \
+    -e 's/^listen-address .*/listen-address  0.0.0.0:'"${PRIVOXY_PORT}"'/' \
+    -e 's/^enforce-blocks .*/#enforce-blocks 0/' \
+    -e 's/^buffer-limit .*/buffer-limit 25600/' \
+    -e 's/^keep-alive-timeout .*/keep-alive-timeout 120/' \
+    -e 's/^tolerate-pipelining .*/tolerate-pipelining 0/' \
+    -e 's/^socket-timeout .*/socket-timeout 30/' \
+    -e 's/^#max-client-connections .*/max-client-connections 256/' \
+    -e 's/^#listen-backlog .*/listen-backlog 128/' \
+    -e 's/^#ca-directory .*/ca-directory \/usr\/local\/etc\/privoxy\/CA/' \
+    -e 's/^#ca-cert-file .*/ca-cert-file privoxy-ca-bundle.crt/' \
+    -e 's/^#ca-key-file .*/ca-key-file cakey.pem/' \
+    -e 's/^#certificate-directory .*/certificate-directory \/usr\/local\/etc\/privoxy\/certs/' \
+    -e 's/^#trusted-cas-file .*/trusted-cas-file trustedCAs.pem/' \
     -e '$a\receive-buffer-size 32768' \
     -e '$a\cipher-list ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256' \
   /usr/local/etc/privoxy/config
--- tests/test_privoxy.py
+++ tests/test_privoxy.py
@@ -11,34 +11,34 @@ class TestPrivoxyContainer:
     def _is_blocked_by_privoxy(self, response):
         return response.status_code == 403 and "Request blocked" in response.text
 
-    # def test_http(self, docker_privoxy, make_request):
-    #     resp = make_request("http://google.com")
-    #     assert resp.status_code == 200
-    #     assert "google" in resp.text
+    def test_http(self, docker_privoxy, make_request):
+        resp = make_request("http://google.com")
+        assert resp.status_code == 200
+        assert "google" in resp.text
 
-    # def test_https(self, docker_privoxy, make_request):
-    #     resp = make_request("https://google.com")
-    #     assert resp.status_code == 200
-    #     assert "google" in resp.text
+    def test_https(self, docker_privoxy, make_request):
+        resp = make_request("https://google.com")
+        assert resp.status_code == 200
+        assert "google" in resp.text
 
-    # def test_https_no_verify(self, docker_privoxy, make_request):
-    #     with pytest.raises(SSLError):
-    #         make_request("https://google.com", use_privoxy_ca_bundle=False)
+    def test_https_no_verify(self, docker_privoxy, make_request):
+        with pytest.raises(SSLError):
+            make_request("https://google.com", use_privoxy_ca_bundle=False)
 
-    # def test_adblock_filters(self, docker_privoxy, make_request):
-    #     resp = make_request("http://googie-anaiytics.com")
-    #     assert self._is_blocked_by_privoxy(resp) == True
-    #     resp = make_request("https://googie-anaiytics.com")
-    #     assert self._is_blocked_by_privoxy(resp) == True
+    def test_adblock_filters(self, docker_privoxy, make_request):
+        resp = make_request("http://googie-anaiytics.com")
+        assert self._is_blocked_by_privoxy(resp) == True
+        resp = make_request("https://googie-anaiytics.com")
+        assert self._is_blocked_by_privoxy(resp) == True
 
-    # def test_no_adblock_filters(self):
-    #     try:
-    #         resp = requests.get("http://googie-anaiytics.com")
-    #         assert self._is_blocked_by_privoxy(resp) == False
-    #         resp = requests.get("https://googie-anaiytics.com")
-    #         assert self._is_blocked_by_privoxy(resp) == False
-    #     except ConnectionError:
-    #         pass # 99% blocked by external software
+    def test_no_adblock_filters(self):
+        try:
+            resp = requests.get("http://googie-anaiytics.com")
+            assert self._is_blocked_by_privoxy(resp) == False
+            resp = requests.get("https://googie-anaiytics.com")
+            assert self._is_blocked_by_privoxy(resp) == False
+        except ConnectionError:
+            pass  # 99% blocked by external software
 
     def test_http_adblock_css_filters(self, docker_privoxy, make_request, env_info):
         resp = requests.get(f"http://{env_info['ip']}/ab2p.common.css")
@@ -51,18 +51,18 @@ class TestPrivoxyContainer:
         )
         assert resp.status_code == 200
 
-    # def test_privman_blocklist(self, docker_privoxy, make_request, exec_privman):
-    #     resp = exec_privman(docker_privoxy, "--add-blocklist", ".google.")
-    #     assert "successfully" in resp
-    #     time.sleep(3)
-    #     resp = make_request("http://google.com")
-    #     assert self._is_blocked_by_privoxy(resp) == True
-    #     resp = make_request("https://google.com")
-    #     assert self._is_blocked_by_privoxy(resp) == True
-    #     resp = exec_privman(docker_privoxy, "--remove-blocklist", ".google.")
-    #     assert "successfully" in resp
-    #     time.sleep(3)
-    #     resp = make_request("http://google.com")
-    #     assert resp.status_code == 200
-    #     resp = make_request("https://google.com")
-    #     assert resp.status_code == 200
+    def test_privman_blocklist(self, docker_privoxy, make_request, exec_privman):
+        resp = exec_privman(docker_privoxy, "--add-blocklist", ".google.")
+        assert "successfully" in resp
+        time.sleep(3)
+        resp = make_request("http://google.com")
+        assert self._is_blocked_by_privoxy(resp) == True
+        resp = make_request("https://google.com")
+        assert self._is_blocked_by_privoxy(resp) == True
+        resp = exec_privman(docker_privoxy, "--remove-blocklist", ".google.")
+        assert "successfully" in resp
+        time.sleep(3)
+        resp = make_request("http://google.com")
+        assert resp.status_code == 200
+        resp = make_request("https://google.com")
+        assert resp.status_code == 200
